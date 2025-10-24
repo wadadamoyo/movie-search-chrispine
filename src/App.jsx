5e97f05915443f7e1d602beafbd18a91ff457358
Wadada Moyo <chrismoyo6@gmail.com>
2025-10-24 21:42:23 +0200

    feat: added tagline and button

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function MovieSite() {
+
+  return (
+    <>
+       <div className="container">
+          <h1>Your Gateway to the world of Movies.</h1>
+          <a href="">watch more</a>
+       </div>
+    </>
+  )
+}
+
+export default MovieSite

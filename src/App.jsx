c72f0dc3ab245527de52cbbeaadc8bd3045f8e08
Wadada Moyo <chrismoyo6@gmail.com>
2025-10-25 06:54:37 +0200

    feat: added header section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,18 @@ function MovieSite() {
 
   return (
     <>
+    <div className="header">
+      <div className="logo">
+        Chris Movies
+      </div>
+      <div className='headerLinks'>
+        <button>
+          <i className='fa-solid fa-search'></i>
+        </button>
+        <a  className='log' href="">Log in</a>
+        <a className='sub' href="">Subscribe</a>
+      </div>
+    </div>
        <div className="container">
           <h1 className='tagline'>Your Gateway to the world of Movies.</h1>
           <a className="watch-btn" href="">watch more</a>
